8a78507ea2ba3e29a2e3a125ed0512d409c7d540
Aaron Parsons <aaron.parsons@live.ca>
2018-09-11 15:54:09 +0000

    added form validation and changed createTweetElement to protect from XSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,32 +6,37 @@
 
 $('document').ready(function(){
 
-  function renderTweets(tweets) {
-    for (let i in tweets) {
-      const currentTweet = createTweetElement(tweets[i]);
-      $('.tweet-container').append(currentTweet);
-    }
-  }
-  
-
-  function createTweetElement(tweetData) {
-    const parent =  $('<article>').addClass('tweet');
-    const header = $('<header>').html(`<img src="${tweetData.user.avatars.small}"><h3>${tweetData.user.name}</h3><p>${tweetData.user.handle}</p>`);
-    const content = $('<p>').addClass('tweet-content').html(`${tweetData.content.text}`);
-    const footer = $('<footer>').html(`<span>${tweetData.created_at}</span><span class="tweet-icons"><i class="fas fa-flag icons"></i><i class="fas fa-retweet icons"></i><i class="fas fa-heart icons"></i></span>`);
-    return $(parent).append(header).append(content).append(footer);
-  }
-
-
   $('#tweet-submit').on('submit', function (event) {
     event.preventDefault();
     console.log('Button clicked, performing ajax call...');
-    const text = $(this).serialize();
-    $.ajax('/tweets', {method: "POST", data: text})
-    .then(function() {
-      console.log('Success');
-    });
+    if (!invalidTweet()) {
+      const text = $(this).serialize();
+      $.ajax('/tweets', {method: "POST", data: text})
+      .then(function() {
+        console.log('Success');
+        $('#new-tweet-textarea').val('');
+        loadTweets();
+      });
+    } else {
+      const error = invalidTweet();
+      alert(error);
+    }
   });
+
+  function renderTweets(tweets) {
+    for (let i in tweets) {
+      const currentTweet = createTweetElement(tweets[i]);
+      $('.tweet-container').prepend(currentTweet);
+    }
+  }
+  
+  function createTweetElement(tweetData) {
+    const parent =  $('<article>').addClass('tweet');
+    const header = $('<header>').html(`<img src="${tweetData.user.avatars.small}"><h3>${tweetData.user.name}</h3><p>${tweetData.user.handle}</p>`);
+    const content = $('<p>').addClass('tweet-content').text(`${tweetData.content.text}`);
+    const footer = $('<footer>').html(`<span>${tweetData.created_at}</span><span class="tweet-icons"><i class="fas fa-flag icons"></i><i class="fas fa-retweet icons"></i><i class="fas fa-heart icons"></i></span>`);
+    return $(parent).append(header).append(content).append(footer);
+  }
 
   function loadTweets() {
     $.ajax('/tweets', { method: 'GET' })
@@ -39,6 +44,17 @@ $('document').ready(function(){
       console.log('Success: ', tweets);
       renderTweets(tweets);
     });
+  }
+
+  function invalidTweet() {
+    const input = $('#new-tweet-textarea').val();
+    if (input === '') { 
+      return 'Tweet text is empty!';
+    } else if (input.length > 140) {
+      return 'Tweet is over 140 characters!';
+    } else {
+      return false;
+    }
   }
 
   loadTweets();
