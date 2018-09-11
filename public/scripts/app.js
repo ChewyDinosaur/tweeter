052bbc583899704086291efb49b9cb2fb05b3f81
Aaron Parsons <aaron.parsons@live.ca>
2018-09-11 18:16:52 +0000

    added compose button that toggles new tweet section and auto targets the text area

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function renderTweets(tweets) {
+  for (let i in tweets) {
+    const currentTweet = createTweetElement(tweets[i]);
+    $('.tweet-container').prepend(currentTweet);
+  }
+}
+
+function createTweetElement(tweetData) {
+  const parent =  $('<article>').addClass('tweet');
+  const header = $('<header>').html(`<img src="${tweetData.user.avatars.small}"><h3>${tweetData.user.name}</h3><p>${tweetData.user.handle}</p>`);
+  const content = $('<p>').addClass('tweet-content').text(`${tweetData.content.text}`);
+  const footer = $('<footer>').html(`<span>${tweetData.created_at}</span><span class="tweet-icons"><i class="fas fa-flag icons"></i><i class="fas fa-retweet icons"></i><i class="fas fa-heart icons"></i></span>`);
+  return $(parent).append(header).append(content).append(footer);
+}
+
+function loadTweets() {
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function (tweets) {
+    console.log('Success: ', tweets);
+    renderTweets(tweets);
+  });
+}
+
+function invalidTweet() {
+  const input = $('#new-tweet-textarea').val();
+  if (input === '') { 
+    return 'Tweet text is empty!';
+  } else if (input.length > 140) {
+    return 'Tweet is over 140 characters!';
+  } else {
+    return false;
+  }
+}
+
+
+
 $('document').ready(function(){
 
   $('#tweet-submit').on('submit', function (event) {
@@ -23,39 +59,13 @@ $('document').ready(function(){
     }
   });
 
-  function renderTweets(tweets) {
-    for (let i in tweets) {
-      const currentTweet = createTweetElement(tweets[i]);
-      $('.tweet-container').prepend(currentTweet);
-    }
-  }
-  
-  function createTweetElement(tweetData) {
-    const parent =  $('<article>').addClass('tweet');
-    const header = $('<header>').html(`<img src="${tweetData.user.avatars.small}"><h3>${tweetData.user.name}</h3><p>${tweetData.user.handle}</p>`);
-    const content = $('<p>').addClass('tweet-content').text(`${tweetData.content.text}`);
-    const footer = $('<footer>').html(`<span>${tweetData.created_at}</span><span class="tweet-icons"><i class="fas fa-flag icons"></i><i class="fas fa-retweet icons"></i><i class="fas fa-heart icons"></i></span>`);
-    return $(parent).append(header).append(content).append(footer);
-  }
+  $('#compose-btn').click(function(e) {
+    $('.new-tweet').slideToggle('slow', function() {
+      // Animation complete.
+      $('#new-tweet-textarea').focus();
 
-  function loadTweets() {
-    $.ajax('/tweets', { method: 'GET' })
-    .then(function (tweets) {
-      console.log('Success: ', tweets);
-      renderTweets(tweets);
     });
-  }
-
-  function invalidTweet() {
-    const input = $('#new-tweet-textarea').val();
-    if (input === '') { 
-      return 'Tweet text is empty!';
-    } else if (input.length > 140) {
-      return 'Tweet is over 140 characters!';
-    } else {
-      return false;
-    }
-  }
+  });
 
   loadTweets();
 });
